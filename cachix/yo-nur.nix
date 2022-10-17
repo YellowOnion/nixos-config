ebd62907e1db3179e19cdef12209387eaa6759a8
Daniel Hill <daniel@gluo.nz>
2022-10-17 14:20:56 +1300

    bump

---
+++ cachix/yo-nur.nix
@@ -0,0 +1,11 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://yo-nur.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "yo-nur.cachix.org-1:E/RHfQMAZ90mPhvsaqo/GrQ3M1xzXf5Ztt0o+1X3+Bs="
+    ];
+  };
+}
